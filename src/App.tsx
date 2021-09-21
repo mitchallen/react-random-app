8f6d151f176139a082a90019752dceda43eedcda
Mitch Allen <github@mitchallen.com>
2021-09-21 06:31:53 -0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
 import './App.css';
+import { Card, CardActions, CardContent, Fab, FormControl, FormHelperText, TextField, Typography } from '@mui/material';
+import { AppStyles } from './AppStyles';
+import { Refresh, SmartToy } from '@mui/icons-material';
+import { Chance } from 'chance';
+
+const chance = new Chance();
+
+interface TextInput {
+  id?: string,
+  name?: string,
+  label?: string,
+  helperText?: string,
+  init?: string,
+}
+
+function useTextInput(options: TextInput = {}) {
+  let {
+    id = undefined,
+    name = undefined,
+    label = 'Text',
+    helperText = 'Enter text',
+    init = '',
+  } = options;
+
+  if (name === undefined) {
+    name = id;
+  }
+
+  const [value, setValue] = useState(init);
+  const handleValueChange = useCallback((event) => {
+    setValue(event.target.value);
+  }, []);
+
+  return {
+    id,
+    name,
+    label,
+    value,
+    onChange: handleValueChange,
+    helperText,
+  }
+}
+
+function useMinimumInput() {
+  return useTextInput({
+    init: '0',
+    label: 'Minimum',
+    helperText: 'Enter minimum'
+  });
+}
+
+function useMaximumInput() {
+  return useTextInput({
+    init: '100',
+    label: 'Maximum',
+    helperText: 'Enter maximum'
+  });
+}
+
+function getRandom(min: number, max: number) {
+
+  if (Number.isNaN(min)) return 0;
+  if (Number.isNaN(max)) return 0;
+  if (min > max) return 0;
+
+  return chance.integer({
+    min: Math.round(min),
+    max: Math.round(max)
+  });
+}
 
 function App() {
+
+  let minimum = useMinimumInput();
+  let maximum = useMaximumInput();
+
+  let [random, setRandom] = useState(0);
+
+  // this will generate a new value every time min or max is changed:
+  random = getRandom(+minimum.value, +maximum.value);
+
+  const handleRefresh = useCallback((event) => {
+      setRandom(getRandom(+minimum.value, +maximum.value));
+  }, [minimum, maximum])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Card style={AppStyles.cardStyle}>
+      <CardContent>
+        <SmartToy style={AppStyles.logoStyle} fontSize="large" />
+        <Typography gutterBottom variant="h5" component="h2">
+          Random Number Generator
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <FormControl style={{ margin: '10px', width: '40%' }} >
+          <TextField
+            {...minimum}
+          />
+        </FormControl>
+        <FormControl style={{ margin: '10px', width: '40%' }} >
+          <TextField
+            {...maximum}
+          />
+        </FormControl>
+        <FormControl style={{ margin: '10px', width: '100%' }} >
+          <Typography gutterBottom variant="h5" component="h2">
+            {random}
+          </Typography>
+        </FormControl>
+      </CardContent>
+      <CardActions style={AppStyles.childActionsStyle}>
+        <Fab
+          color="primary"
+          aria-label="refresh"
+          onClick={handleRefresh}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Refresh />
+        </Fab>
+      </CardActions>
+    </Card>
   );
 }
 
---
+++ src/AppStyles.tsx
@@ -0,0 +1,22 @@
+export const AppStyles: any = {
+    cardStyle: {
+        width: '400px',
+        margin: 'auto',
+        marginTop: 20,
+        padding: '10px',
+        textAlign: 'center',
+        display: 'block',
+    },
+    childActionsStyle: {
+        padding: '20px',
+        textAlign: 'right',
+        display: 'block',
+    },
+    logoStyle: { 
+        marginTop: '20px', 
+        marginBottom: '20px', 
+        padding: '10px', 
+        border: '1px solid black', 
+        borderRadius: '50%' 
+    }
+}
