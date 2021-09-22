24ec2426f9c49285d9b3dde9168bbef46c71f8be
Mitch Allen <github@mitchallen.com>
2021-09-22 05:46:50 -0400

    added clipboard copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useState } from 'react';
 import './App.css';
-import { Card, CardActions, CardContent, Fab, FormControl, FormHelperText, TextField, Typography } from '@mui/material';
+import { Card, CardActions, CardContent, Fab, FormControl, Snackbar, TextField, Typography } from '@mui/material';
 import { AppStyles } from './AppStyles';
-import { Refresh, SmartToy } from '@mui/icons-material';
+import { ContentCopy, Refresh, SmartToy } from '@mui/icons-material';
 import { Chance } from 'chance';
 
 const chance = new Chance();
@@ -73,64 +73,104 @@ function getRandom(min: number, max: number) {
 
 function App() {
 
+  // snackbar
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const handleCloseSnackbar = useCallback(() => {
+    setSnackbarOpen(false);
+  }, []);
+
+
   let minimum = useMinimumInput();
   let maximum = useMaximumInput();
 
-  let [random, setRandom] = useState(0);
+  let [random, setRandom] = useState(getRandom(+minimum.value, +maximum.value));
 
   // this will generate a new value every time min or max is changed:
   // Do NOT use setRandom here or you will get "Too many re-renders";
-  random = getRandom(+minimum.value, +maximum.value);
+  // random = getRandom(+minimum.value, +maximum.value);
 
   const handleRefresh = useCallback((event) => {
     setRandom(getRandom(+minimum.value, +maximum.value));
-  }, []);
+  }, [minimum.value, maximum.value]);
+
+  const handleCopy = useCallback((event) => {
+    navigator.clipboard.writeText(`${random}`).then(function() {
+      /* clipboard successfully set */
+      setSnackbarOpen(true);
+      setSnackbarMessage(`Copied '${random}' to clipboard`);
+    }, function() {
+      /* clipboard write failed */
+      setSnackbarOpen(true);
+      setSnackbarMessage(`Error: could not copy to clipboard`);
+    });
+  }, [random]);
 
   return (
-    <Card style={AppStyles.cardStyle}>
-      <CardContent>
-        <SmartToy style={AppStyles.logoStyle} fontSize="large" />
-        <Typography gutterBottom variant="h5" component="h2">
-          Random Number Generator
-        </Typography>
-      </CardContent>
-      <CardContent>
-        <FormControl style={{ margin: '10px', width: '40%' }} >
-          <TextField
-            {...minimum}
-          />
-        </FormControl>
-        <FormControl style={{ margin: '10px', width: '40%' }} >
-          <TextField
-            {...maximum}
-          />
-        </FormControl>
-        <FormControl style={{ marginTop: '30px', margin: '10px', width: '40%' }} >
-          <TextField
-            type='text'
-            value={random}
-            label='Random number'
-            helperText='Copy to clipboard'
-            variant='outlined'
-            inputProps={
-              { readOnly: true, }
-            }
-          />
-          {/* <Typography gutterBottom variant="h5" component="h2">
+    <React.Fragment>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={snackbarOpen}
+        onClose={handleCloseSnackbar}
+        message={snackbarMessage}
+        autoHideDuration={6000}
+      />
+      <Card style={AppStyles.cardStyle}>
+        <CardContent>
+          <SmartToy style={AppStyles.logoStyle} fontSize="large" />
+          <Typography gutterBottom variant="h5" component="h2">
+            Random Number Generator
+          </Typography>
+        </CardContent>
+        <CardContent>
+          <FormControl style={{ margin: '10px', width: '40%' }} >
+            <TextField
+              {...minimum}
+            />
+          </FormControl>
+          <FormControl style={{ margin: '10px', width: '40%' }} >
+            <TextField
+              {...maximum}
+            />
+          </FormControl>
+          <FormControl style={{ marginTop: '30px', margin: '10px', width: '40%' }} >
+            <TextField
+              type='text'
+              value={random}
+              label='Random number'
+              helperText='Copy to clipboard'
+              variant='outlined'
+              inputProps={
+                { readOnly: true, }
+              }
+            />
+            {/* <Typography gutterBottom variant="h5" component="h2">
             {random}
           </Typography> */}
-        </FormControl>
-      </CardContent>
-      <CardActions style={AppStyles.childActionsStyle}>
-        <Fab
-          color="primary"
-          aria-label="refresh"
-          onClick={handleRefresh}
-        >
-          <Refresh />
-        </Fab>
-      </CardActions>
-    </Card>
+          </FormControl>
+        </CardContent>
+        <CardActions style={AppStyles.childActionsStyle}>
+          <Fab
+            color="secondary"
+            size="medium"
+            aria-label="copy"
+            onClick={handleCopy}
+          >
+            <ContentCopy />
+          </Fab>
+          <Fab
+            color="primary"
+            aria-label="refresh"
+            onClick={handleRefresh}
+          >
+            <Refresh />
+          </Fab>
+        </CardActions>
+      </Card>
+    </React.Fragment>
   );
 }
 
